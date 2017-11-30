8ba644b676de99f71f9b520ac694976562da43d0
Cesar Gutierrez <cesar@tineo.mobi>
2017-11-30 08:06:44 -0500

    fix TipoPagoServImp

--- src/main/java/service/TipoPagoServiceImpl.java
+++ src/main/java/service/TipoPagoServiceImpl.java
@@ -2,8 +2,31 @@ package service;
 
 import dao.ConductorDAO;
 import dao.FactoryDAO;
+import entity.TipoPago;
+
+import java.util.List;
 
 public class TipoPagoServiceImpl implements TipoPagoService {
     FactoryDAO factory = FactoryDAO.getFactory(FactoryDAO.MYSQL_FACTORY);
     ConductorDAO conductorDAO = factory.getConductorDAO();
+
+    @Override
+    public int insertarTipo(TipoPago tipoPago) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public List<TipoPago> listarTipo() throws Exception {
+        return null;
+    }
+
+    @Override
+    public int eliminarTipo(int codigo) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public List<TipoPago> buscarTipo(TipoPago tipoPago) throws Exception {
+        return null;
+    }
 }

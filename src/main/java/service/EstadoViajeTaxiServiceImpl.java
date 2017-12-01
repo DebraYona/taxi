929cb4092564862445737486045943b1d1ab57af
2hubrulee <alejandro2hpxa@gmail.com>
2017-12-01 09:08:47 +0000

    factoria remix

--- src/main/java/service/EstadoViajeTaxiServiceImpl.java
+++ src/main/java/service/EstadoViajeTaxiServiceImpl.java
@@ -1,9 +1,33 @@
 package service;
 
 import dao.ConductorDAO;
+import dao.EstadoViajeTaxiDAO;
 import dao.FactoryDAO;
+import entity.EstadoViajeTaxi;
+
+import java.util.List;
 
 public class EstadoViajeTaxiServiceImpl implements EstadoViajeTaxiService {
     FactoryDAO factory = FactoryDAO.getFactory(FactoryDAO.MYSQL_FACTORY);
-    ConductorDAO conductorDAO = factory.getConductorDAO();
+    EstadoViajeTaxiDAO estadoViajeTaxiDAO = factory.getEstadoViajeTaxiDAO();
+
+    @Override
+    public int insertarEstadoViajeTaxi(EstadoViajeTaxi estadoViajeTaxi) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int eliminarEstadoViajeTaxi(int codigo) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public List<EstadoViajeTaxi> listarEstadoViajeTaxi() throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<EstadoViajeTaxi> buscarEstadoViajeTaxi(EstadoViajeTaxi estadoViajeTaxi) throws Exception {
+        return null;
+    }
 }

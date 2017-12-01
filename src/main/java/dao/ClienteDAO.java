5108600121083f0b58acb050d13ca1da99f523ff
2hubrulee <alejandro2hpxa@gmail.com>
2017-12-01 04:23:05 +0000

    magia en el dao de clientes

--- src/main/java/dao/ClienteDAO.java
+++ src/main/java/dao/ClienteDAO.java
@@ -6,18 +6,13 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface ClienteDAO {
-
-
-
-
         Cliente buscarCliente(String nombrecliente);
-
-        ArrayList<Cliente> listarClientes(Cliente cliente) throws SQLException;
+        ArrayList<Cliente> listarClientes() throws SQLException;
         int insertarCliente(Cliente cliente);
-        void modificarCliente(Cliente cliente);
+        int modificarCliente(Cliente cliente) throws Exception;
         int eliminarCliente(int codigo) throws Exception;
-      /*  boolean validarCliente(String username, String password);*/
-    }
+        //boolean validarCliente(String username, String password);
+}
 
 
 
